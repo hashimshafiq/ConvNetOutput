81a51d866b6c6b13bacb15f8d0a0f3ca04a78c68
hashimshafiq <hashimshafiq0@gmail.com>
2017-11-04 22:39:25 +0500

    funcionality added

--- app/src/main/java/convnet/hashim/com/convnetoutput/MainActivity.java
+++ app/src/main/java/convnet/hashim/com/convnetoutput/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button calcbtn = null;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText noFText = null;
     EditText strideText = null;
     EditText paddingText = null;
+
+    int height=0,width=0,depth=0;
+    int size=0,number=0,stride=1,pad=0;
 
 
     @Override
@@ -43,10 +47,108 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
     }
 
     @Override
     public void onClick(View view) {
+        if(view.getId()==R.id.calculateBtn){
+            getValues();
+            int h = returnHeight(height,size,stride,pad);
+            int w = returnWidth(width,size,stride,pad);
+            int d = returnDepth(number);
+            String output = " ( "+h+" , "+w+" , "+d+" ) ";
+            Toast.makeText(this,output,Toast.LENGTH_LONG).show();
+
+        }
 
     }
+
+    public int returnHeight(int height,int f,int s,int p){
+        int pad = 2*p;
+        int total = ((height+pad-f)/s)+1;
+        return total;
+    }
+
+    public int returnWidth(int width, int f,int s,int p){
+        int pad = 2*p;
+        int total = ((width+pad-f)/s)+1;
+        return total;
+    }
+
+    public int returnDepth(int numOfFilter){
+
+        return numOfFilter;
+    }
+
+    public void getValues(){
+        //input layer
+        String h = heightText.getText().toString();
+        if(!h.isEmpty()){
+            height = Integer.parseInt(h);
+        }else{
+            heightText.setText("0");
+            height = 0;
+        }
+
+        String w = widthText.getText().toString();
+
+        if(!w.isEmpty()) {
+            width = Integer.parseInt(w);
+        }else{
+            widthText.setText("0");
+            width = 0;
+        }
+
+        String d = depthText.getText().toString();
+
+        if(!d.isEmpty()) {
+            depth = Integer.parseInt(d);
+        }else{
+            depthText.setText("0");
+            depth = 0;
+        }
+
+        // convolutional layers
+
+        String si = sizeFText.getText().toString();
+
+        if(!si.isEmpty()) {
+            size = Integer.parseInt(si);
+        }else{
+            sizeFText.setText("0");
+            size = 0;
+        }
+
+        String f = noFText.getText().toString();
+
+        if(!f.isEmpty()) {
+            number = Integer.parseInt(f);
+        }else{
+            noFText.setText("0");
+            number = 0;
+        }
+
+        String st = strideText.getText().toString();
+
+        if(!st.isEmpty()) {
+            stride = Integer.parseInt(st);
+        }else{
+            strideText.setText("1");
+            stride = 1;
+        }
+
+        String p = paddingText.getText().toString();
+
+        if(!p.isEmpty()) {
+            pad = Integer.parseInt(p);
+        }else{
+            paddingText.setText("0");
+            pad = 0;
+        }
+
+
+    }
+
+
 }
