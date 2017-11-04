a9fe1ce2e3e978e8b3c6db51ac3c5c29acfccdd8
hashimshafiq <hashimshafiq0@gmail.com>
2017-11-04 21:40:09 +0500

    funcionality added

--- app/src/main/java/convnet/hashim/com/convnetoutput/MainActivity.java
+++ app/src/main/java/convnet/hashim/com/convnetoutput/MainActivity.java
@@ -2,12 +2,51 @@ package convnet.hashim.com.convnetoutput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button calcbtn = null;
+    Button resetbtn = null;
+    EditText heightText = null;
+    EditText widthText = null;
+    EditText depthText = null;
+    EditText sizeFText = null;
+    EditText noFText = null;
+    EditText strideText = null;
+    EditText paddingText = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // input layers
+        heightText = (EditText) findViewById(R.id.heightText);
+        widthText = (EditText) findViewById(R.id.widthText);
+        depthText = (EditText) findViewById(R.id.depthText);
+
+        //filter layer
+        sizeFText = (EditText) findViewById(R.id.filterText);
+        noFText = (EditText) findViewById(R.id.numberOfFilterText);
+        strideText = (EditText)findViewById(R.id.stridesText);
+        paddingText = (EditText) findViewById(R.id.paddingText);
+
+        // buttons
+        calcbtn = (Button) findViewById(R.id.calculateBtn);
+        calcbtn.setOnClickListener(this);
+        resetbtn = (Button) findViewById(R.id.resetBtn);
+        resetbtn.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
